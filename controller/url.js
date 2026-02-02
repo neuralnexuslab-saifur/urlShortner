7d7ef00e0d625c89b5a1d01258beb654c8687ab7
nueralnexuslab-saifur <nueralnexuslab.saifur@gmail.com>
2026-02-02 17:21:24 +0530

    db created success

--- controller/url.js
+++ controller/url.js
@@ -1,10 +1,17 @@
 const shortId = require("short-id")
+const URL = require("../model/url.js")
 //shortId.configure({ length: 8 })
 async function handleGenerateNewShortUrl(req, res) {
+    const body = req.body
     const shortString = shortId.generate()
+    await URL.create({
+        shortId: shortString,
+        redirectUrl: body.url,
+        visitHistory: []
+    })
     return res.json({ str: shortString })
 }
 
 module.exports = {
     handleGenerateNewShortUrl
-}
+} 
--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express()
 const urlRoute = require("./routes/url.js")
 const { connectToMongoDb } = require("./connection.js")
 const PORT = 8001
-connectToMongoDb("mongodb://127.0.0.1:27017/short-url")
+connectToMongoDb("mongodb://localhost:27017/short-url")
     .then(() => console.log("mongo conntected Saifur"))
-
+app.use(express.json())
 app.use("/url", urlRoute)
 app.listen(PORT, ()=> console.log("Server started listening at PORT "))
